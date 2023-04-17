0db75014638de39ae9e362728ee0b1fb5c3f5d3e
Anatoliy-13 <arhangeltm13@gmail.com>
2023-04-17 19:35:45 +0200

    add carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,8 +18,14 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (typeof sampleActivity !== 'string' || !Number(sampleActivity) || sampleActivity < 1 || sampleActivity > MODERN_ACTIVITY) {
+    return false;
+ } else {
+    let K = 0.693 / HALF_LIFE_PERIOD;
+    return Math.ceil((Math.log(MODERN_ACTIVITY / Number(sampleActivity))) / K);
+ }
 }
 
 module.exports = {
